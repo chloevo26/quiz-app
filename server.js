bcd94240bfb8b760e2bc13d8551de7ab9c041118
Chloe Vo <chloevo@Chloes-MBP-2.attlocal.net>
2020-03-08 18:20:45 -0700

    inital commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+
+})
+
+
+const PORT = 8080 || process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
