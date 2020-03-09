df371d5d3c48969008aea74303efcb75a0cadd4f
Chloe Vo <chloevo@Chloes-MBP-2.attlocal.net>
2020-03-08 18:29:21 -0700

    update port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
